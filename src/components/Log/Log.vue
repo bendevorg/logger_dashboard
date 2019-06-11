fadae9c70d931f12f409e3949e21709a5af96f83
Guilherme Conti <guibasconti@gmail.com>
2019-06-10 21:02:25 -0300

    Improve log spacing

--- src/components/Log/Log.vue
+++ src/components/Log/Log.vue
@@ -9,34 +9,49 @@
     }"
     @click="toggle(index)"
   >
-    <v-list-tile-content>
-      <div>
-        <h3><v-icon>alarm</v-icon> {{ time }}</h3>
-      </div>
-      <v-chip v-if="status == 200 || status == 201" color="green" text-color="white"  small="true">
-        {{ status }}
-      </v-chip>
-      <v-chip v-else color="red" text-color="white"  small="true">
-        {{ status }}
-      </v-chip>  
-    </v-list-tile-content>
+    <v-layout
+      row
+      align-center
+      justify-space-between
+      wrap
+    >
+      <v-flex xs4 sm2>
+        <v-list-tile-content>
+          <div>
+            <h3><v-icon>alarm</v-icon> {{ time }}</h3>
+          </div>
+          <v-chip v-if="status == 200 || status == 201" color="green" text-color="white"  small>
+            {{ status }}
+          </v-chip>
+          <v-chip v-else color="red" text-color="white" small>
+            {{ status }}
+          </v-chip>  
+        </v-list-tile-content>
+      </v-flex>
 
-    <v-list-tile-content>
-      <v-chip label color="white" text-color="black">
-        {{ method }}
-      </v-chip>
-    </v-list-tile-content>
+      <v-flex xs4 sm2>
+        <v-list-tile-content>
+          <v-chip label color="white" text-color="black">
+            {{ method }}
+          </v-chip>
+        </v-list-tile-content>
+      </v-flex>
 
-    <v-list-tile-content>
-      <v-list-tile-title>{{ app }}</v-list-tile-title>
-      <v-list-tile-sub-title>{{ path }}</v-list-tile-sub-title>
-    </v-list-tile-content>
+      <v-flex xs12 sm6>
+        <v-list-tile-content>
+          <v-list-tile-title>{{ app }}</v-list-tile-title>
+          <v-list-tile-sub-title>{{ path }}</v-list-tile-sub-title>
+        </v-list-tile-content>
+      </v-flex>
 
-    <v-list-tile-action>
-      <v-chip label color="orange" text-color="white">
-        <v-icon>av_timer</v-icon> {{ responseTime }}ms
-      </v-chip>
-    </v-list-tile-action>
+      <v-flex xs4 sm2>
+        <v-list-tile-action>
+          <v-chip label color="orange" text-color="white">
+            <v-icon>av_timer</v-icon> {{ responseTime }}ms
+          </v-chip>
+        </v-list-tile-action>
+      </v-flex>
+    </v-layout>
   </v-list-tile>
 </template>
 
@@ -75,11 +90,6 @@ export default {
       default: 0,
     },
     index: {
-      type: Number,
-      required: true,
-      default: 0,
-    },
-    length: {
       type: Number,
       required: true,
       default: 0,
